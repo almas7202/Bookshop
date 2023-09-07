d27372d8f71307d265b6e7d6f429889c83ff9854
almas7202 <almasshaikh07202@gmail.com>
2023-09-07 15:10:19 +0530

    Done something

--- BookApp/views.py
+++ BookApp/views.py
@@ -2,14 +2,44 @@ from django.shortcuts import render,redirect
 from .form import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+import requests
+import random
+
+
+# GOOGLE_BOOKS_API_KEY = 'AIzaSyDqa0wRKCEGjuh9PqBAlqWNvNvxpN-4YNI'
+
 
 
 def baseview(request):
     return render(request,'base.html')
 
 
-def indexview(request):
-    return render(request,'index.html')
+def indexview(request): 
+    google_books_api_key = 'AIzaSyBOgiz9kM8HnX1vtgT8106HgGSOUQ2e7Y4'
+    url = f'https://www.googleapis.com/books/v1/volumes?q=Harry+Potter&key={google_books_api_key}&maxResults=10'
+    
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract the list of books from the API response
+    books = data.get('items', [])
+
+
+    categories = ['comedy', 'adventure', 'thriller']
+    random_category = random.choice(categories)  # Select a random category
+
+    # Construct the API URL with the selected category
+    url1 = f'https://www.googleapis.com/books/v1/volumes?q=subject:{random_category}&key={google_books_api_key}&maxResults=10'
+
+    response = requests.get(url1)
+    data = response.json()
+
+    # Extract the list of books from the API response
+    r_books = data.get('items', [])
+
+    context = {'books': books,'r_books':r_books}
+
+    return render(request,'index.html',context)
 
 def registerview(request):
     if request.method == 'POST':
